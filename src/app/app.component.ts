2a2c854e8f77f6525d88ab2577887d70a9b0f8aa
jayadharreddy <jayadharreddy65538@gmail.com>
2023-07-16 13:29:18 +0530

    09-07-2023 ngViewChild, ngAfterVieInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { RoomsComponent } from './rooms/rooms.component';
 
 @Component({
   selector: 'hinv-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'hotelinventoryapp';
 
-  role = 'Admin';
+ // role = 'Admin';
+  @ViewChild('user', {read: ViewContainerRef}) vcr!: ViewContainerRef;
+  ngAfterViewInit() {
+    const componentRef=this.vcr.createComponent(RoomsComponent);
+    componentRef.instance.numberOfRooms=50;
+  }
+
 }
